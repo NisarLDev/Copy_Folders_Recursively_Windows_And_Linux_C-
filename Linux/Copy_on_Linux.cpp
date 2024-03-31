b4da1916fce1f5d809834f0912e2d7b524dde5ed
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-31 15:46:28 +0200

    Added return 0; at the end of code in Copy_on_Linux.cpp

--- Linux/Copy_on_Linux.cpp
+++ Linux/Copy_on_Linux.cpp
@@ -42,6 +42,6 @@ int main(){
 //     std::error_code ec;
 //     std::filesystem::copy_file(from, to, ec); // does not throw
 //     std::cout << "Non-throwing form sets error_code: " << ec.message() << '\n';
-     
+       return 0;
 }
 
